e09556382d50756ef6edb3e8b5958c8ce4dc0b5d
akkas <akkassingh@gmail.com>
2020-02-19 19:33:49 +0530

    using interfaces.

--- index.ts
+++ index.ts
@@ -1,23 +1,27 @@
-import  * as express from 'express';
-import  * as mongoose from 'mongoose';
-import  * as bodyParser from 'body-parser';
-import  routes from './src/routes/crmRoutes';
+import * as express from 'express';
+import * as mongoose from 'mongoose';
+import * as bodyParser from 'body-parser';
+import routes from './src/routes/crmRoutes';
 import messenger from './src/controllers/createMessage';
-import { Settings } from './settings'
+import { Settings } from './settings';
 
 const app = express();
 
 //instance of our class
 let messages = new messenger(Settings.PORT);
-const dataConnection = (user: string, pass: string,db:string): string =>{
-    return `mongodb://${user}:${pass}@ds219839.mlab.com:19839/${db}`
-}
+const dataConnection = (user: string, pass: string, db: string): string => {
+  return `mongodb://${user}:${pass}@ds219839.mlab.com:19839/${db}`;
+};
 
-let databse = dataConnection(Settings.mlabUser,Settings. mlabPass,Settings.mlabDatabase);
+let databse = dataConnection(
+  Settings.mlabUser,
+  Settings.mlabPass,
+  Settings.mlabDatabase
+);
 
 // mongoose connection
 mongoose.connect(databse, {
-    useMongoClient: true
+  useMongoClient: true
 });
 
 // bodyparser setup
@@ -26,13 +30,21 @@ app.use(bodyParser.json());
 
 routes(app);
 
+interface Name {
+  firstName: string;
+}
+
+//function using interface
+const nameCreator = (name: Name): string => {
+  return `Hello, ${name.firstName} `;
+};
+
+
+let myName = { firstName: 'akkas'};
+
 // serving static files
 app.use(express.static('public'));
 
-app.get('/', (req, res) =>
-    res.send(messages.messagePrint())
-);
+app.get('/', (req, res) => res.send(messages.messagePrint()));
 
-app.listen(Settings.PORT, () =>
-    console.log(messages.messagePrint())
-);
+app.listen(Settings.PORT, () => console.log(nameCreator(myName), messages.messagePrint()));
